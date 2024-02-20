385cf5a5cd66d7565ef370f3dc807916f79d3801
Pablo Lemos <lemosprg@gmail.com>
2024-02-19 23:37:20 -0300

    Adição de confirmação de devolução

--- js/app.js
+++ js/app.js
@@ -2,14 +2,17 @@ function alterarStatus(id){
     let botaoClicado = document.getElementById(`game-${id}`);
     let imagemGame = botaoClicado.querySelector('.dashboard__item__img');
     let botao = botaoClicado.querySelector('.dashboard__item__button');
+    let nomeJogo = botaoClicado.querySelector('.dashboard__item__name');
 
     if (imagemGame.classList.contains('dashboard__item__img--rented')) {
-        imagemGame.classList.remove('dashboard__item__img--rented');
-        botao.textContent = 'Alugar';
-        botao.classList.remove('dashboard__item__button--return');
+        if (confirm(`Você tem certeza que deseja devolver o jogo "${nomeJogo.textContent}"?`)){
+            imagemGame.classList.remove('dashboard__item__img--rented');
+            botao.classList.remove('dashboard__item__button--return');
+            botao.textContent = 'Alugar';
+        }
     } else {
         imagemGame.classList.add('dashboard__item__img--rented');
-        botao.textContent = 'Devolver';
         botao.classList.add('dashboard__item__button--return');
+        botao.textContent = 'Devolver';
     }
 }
